ac1f8ccb310de4241ca6266e5527af37652f0bbe
yekta <ngyekta@gmail.com>
2018-12-30 19:41:55 +0300

    Redundant container removed on intro_welcome

--- lib/ui/intro/intro_welcome.dart
+++ lib/ui/intro/intro_welcome.dart
@@ -17,64 +17,63 @@ class _IntroWelcomePageState extends State<IntroWelcomePage> {
     SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light
         .copyWith(statusBarIconBrightness: Brightness.light));
     return Scaffold(
-        key: _scaffoldKey,
-        backgroundColor: greyLight,
-        body: LayoutBuilder(
-          builder: (context, constraints) => Container(
-                width: double.infinity,
-                child: Column(
-                  children: <Widget>[
-                    //A widget that holds welcome animation + text and expands to the rest of the available area
-                    Expanded(
-                      child: Container(
-                        margin: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.10),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: <Widget>[
-                            //Stuff inside this container except width won't be needed since it'll wrap the animation perfectly.
-                            Container(
-                              color: greyDark,
-                              width: double.infinity,
-                              height: MediaQuery.of(context).size.width * 0.5,
-                              child: Center(child: Text("ANIMATION")),
-                            ),
-                            Container(
-                              margin: EdgeInsets.symmetric(
-                                  horizontal: 50, vertical: 20),
-                              child: Text(
-                                "Welcome to Kalium. To begin you may create a new wallet or import an existing one.",
-                                style: TextStyle(
-                                    fontSize: 16.0,
-                                    fontWeight: FontWeight.w200,
-                                    fontFamily: 'NunitoSans',
-                                    color: white90),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-
-                    //A column with "New Wallet" and "Import Wallet" buttons
-                    Column(
+      key: _scaffoldKey,
+      backgroundColor: greyLight,
+      body: LayoutBuilder(
+        builder: (context, constraints) => Column(
+              children: <Widget>[
+                //A widget that holds welcome animation + text and expands to the rest of the available area
+                Expanded(
+                  child: Container(
+                    margin: EdgeInsets.only(
+                        top: MediaQuery.of(context).size.height * 0.10),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
                       children: <Widget>[
-                        Row(
-                          children: <Widget>[
-                            buildKaliumButton(KaliumButtonType.PRIMARY,
-                                'New Wallet', Dimens.BUTTON_TOP_DIMENS),
-                          ],
+                        //Stuff inside this container except width won't be needed since it'll wrap the animation perfectly.
+                        Container(
+                          color: greyDark,
+                          width: double.infinity,
+                          height: MediaQuery.of(context).size.width * 0.5,
+                          child: Center(child: Text("ANIMATION")),
                         ),
-                        Row(
-                          children: <Widget>[
-                            buildKaliumButton(KaliumButtonType.PRIMARY_OUTLINE,
-                                'Import Wallet', Dimens.BUTTOM_BOTTOM_DIMENS),
-                          ],
+                        Container(
+                          margin: EdgeInsets.symmetric(
+                              horizontal: 50, vertical: 20),
+                          child: Text(
+                            "Welcome to Kalium. To begin you may create a new wallet or import an existing one.",
+                            style: TextStyle(
+                                fontSize: 16.0,
+                                fontWeight: FontWeight.w200,
+                                fontFamily: 'NunitoSans',
+                                color: white90),
+                          ),
                         ),
+                      ],
+                    ),
+                  ),
+                ),
+
+                //A column with "New Wallet" and "Import Wallet" buttons
+                Column(
+                  children: <Widget>[
+                    Row(
+                      children: <Widget>[
+                        buildKaliumButton(KaliumButtonType.PRIMARY,
+                            'New Wallet', Dimens.BUTTON_TOP_DIMENS),
+                      ],
+                    ),
+                    Row(
+                      children: <Widget>[
+                        buildKaliumButton(KaliumButtonType.PRIMARY_OUTLINE,
+                            'Import Wallet', Dimens.BUTTOM_BOTTOM_DIMENS),
                       ],
                     ),
                   ],
                 ),
-              ),
-        ));
+              ],
+            ),
+      ),
+    );
   }
 }
