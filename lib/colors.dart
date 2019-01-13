b3de1b4910d15e212d7c5c1fe66f68d7f227832d
yekta <ngyekta@gmail.com>
2019-01-13 03:11:45 +0300

    Share card creation animation added

--- lib/colors.dart
+++ lib/colors.dart
@@ -28,6 +28,7 @@ class KaliumColors {
   static const white30 = Color(0x4DFFFFFF);
   static const white15 = Color(0x26FFFFFF);
 
+  static const black50 = Color(0x80000000);
   static const black70 = Color(0xB3000000);
   static const black80 = Color(0xCC000000);
 
@@ -59,6 +60,7 @@ class KaliumColors {
   static const text30 = white30;
   static const text15 = white15;
 
+  static const overlay50= black50;
   static const overlay70= black70;
   static const overlay80= black80;
 }
