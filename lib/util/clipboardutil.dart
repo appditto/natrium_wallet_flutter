48a271d9d6609b614e4ebdb29bc740e8c2dd8782
Brandon Berhent <bbedward@gmail.com>
2019-01-18 11:15:23 -0500

    Better clipboard util

--- lib/util/clipboardutil.dart
+++ lib/util/clipboardutil.dart
@@ -2,14 +2,18 @@ import 'package:flutter/services.dart';
 import 'package:flutter_nano_core/flutter_nano_core.dart';
 
 class ClipboardUtil {
-  static bool eventSet = false;
+  static var setStream;
 
   /// Set to clear clipboard after 2 minutes, if clipboard contains a seed
   static Future<void> setClipboardClearEvent() async {
-    if (eventSet) { return; }
-    eventSet = true;
-    await Future.delayed(Duration(minutes: 2), () {
-      eventSet = false;
+    if (setStream != null) {
+      setStream.cancel();
+    }
+    var delayed = new Future.delayed(new Duration(minutes: 2));
+    delayed.then((_) {
+      return true;
+    });
+    setStream = delayed.asStream().listen((_) {
       Clipboard.getData("text/plain").then((data) {
         if (NanoSeeds.isValidSeed(data.text)) {
           Clipboard.setData(ClipboardData(text: ""));
