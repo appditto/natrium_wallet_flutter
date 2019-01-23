f9f06ffec330ed289ab335c19596a750ff550e70
Brandon Berhent <bbedward@gmail.com>
2019-01-23 14:37:12 -0500

    Hookup manual entry

--- lib/ui/transfer/transfer_confirm_sheet.dart
+++ lib/ui/transfer/transfer_confirm_sheet.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:kalium_wallet_flutter/localization.dart';
 import 'package:kalium_wallet_flutter/dimens.dart';
-import 'package:kalium_wallet_flutter/model/vault.dart';
 import 'package:kalium_wallet_flutter/ui/widgets/auto_resize_text.dart';
 import 'package:kalium_wallet_flutter/ui/widgets/sheets.dart';
 import 'package:kalium_wallet_flutter/ui/widgets/buttons.dart';
@@ -10,95 +9,93 @@ import 'package:kalium_wallet_flutter/styles.dart';
 
 class KaliumTransferConfirmSheet {
   mainBottomSheet(BuildContext context) {
-    Vault.inst.getSeed().then((result) {
-      KaliumSheets.showKaliumHeightNineSheet(
-          context: context,
-          builder: (BuildContext context) {
-            return StatefulBuilder(
-                builder: (BuildContext context, StateSetter setState) {
-              return Container(
-                width: double.infinity,
-                child: Column(
-                  mainAxisSize: MainAxisSize.max,
-                  children: <Widget>[
-                    //A container for the header
-                    Container(
-                      margin: EdgeInsets.only(top: 30.0, left:70, right: 70),
-                      child: AutoSizeText(
-                        KaliumLocalization.of(context).transferHeader.toUpperCase(),
-                        style: KaliumStyles.textStyleHeader(context),
-                        textAlign: TextAlign.center,
-                        maxLines: 2,
-                        stepGranularity: 0.1,
-                      ),
+    KaliumSheets.showKaliumHeightNineSheet(
+        context: context,
+        builder: (BuildContext context) {
+          return StatefulBuilder(
+              builder: (BuildContext context, StateSetter setState) {
+            return Container(
+              width: double.infinity,
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                children: <Widget>[
+                  //A container for the header
+                  Container(
+                    margin: EdgeInsets.only(top: 30.0, left:70, right: 70),
+                    child: AutoSizeText(
+                      KaliumLocalization.of(context).transferHeader.toUpperCase(),
+                      style: KaliumStyles.textStyleHeader(context),
+                      textAlign: TextAlign.center,
+                      maxLines: 2,
+                      stepGranularity: 0.1,
                     ),
+                  ),
 
-                    // A container for the paragraphs
-                    Expanded(
-                      child: Container(
-                        margin: EdgeInsets.only(top: MediaQuery.of(context).size.height*0.1),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Container(
-                                margin: EdgeInsets.symmetric(horizontal: smallScreen(context)?35:60),
-                                child: Text(
-                                  KaliumLocalization.of(context).transferConfirmInfo,
-                                  style: KaliumStyles.TextStyleParagraphPrimary,
-                                  textAlign: TextAlign.left,
-                            )),
-                            Container(
-                                margin: EdgeInsets.symmetric(horizontal: smallScreen(context)?35:60),
-                                child: Text(
-                                  KaliumLocalization.of(context).transferConfirmInfoSecond,
-                                  style: KaliumStyles.TextStyleParagraph,
-                                  textAlign: TextAlign.left,
-                            )),
-                            Container(
-                                margin: EdgeInsets.symmetric(horizontal: smallScreen(context)?35:60),
-                                child: Text(
-                                  KaliumLocalization.of(context).transferConfirmInfoThird,
-                                  style: KaliumStyles.TextStyleParagraph,
-                                  textAlign: TextAlign.left,
-                            )),
-                          ],
-                        ),
-                      ),
-                    ),
-                    Container(
+                  // A container for the paragraphs
+                  Expanded(
+                    child: Container(
+                      margin: EdgeInsets.only(top: MediaQuery.of(context).size.height*0.1),
                       child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: <Widget>[
-                          Row(
-                            children: <Widget>[
-                              // Send Button
-                              KaliumButton.buildKaliumButton(
-                                  KaliumButtonType.PRIMARY,
-                                  KaliumLocalization.of(context).confirm.toUpperCase(),
-                                  Dimens.BUTTON_TOP_DIMENS, onPressed: () {
-                                    KaliumTransferCompleteSheet().mainBottomSheet(context);
-                              }),
-                            ],
-                          ),
-                          Row(
-                            children: <Widget>[
-                              // Scan QR Code Button
-                              KaliumButton.buildKaliumButton(
-                                  KaliumButtonType.PRIMARY_OUTLINE,
-                                  KaliumLocalization.of(context).cancel.toUpperCase(),
-                                  Dimens.BUTTON_BOTTOM_DIMENS, onPressed: () {
-                                Navigator.of(context).pop();
-                              }),
-                            ],
-                          ),
+                          Container(
+                              margin: EdgeInsets.symmetric(horizontal: smallScreen(context)?35:60),
+                              child: Text(
+                                KaliumLocalization.of(context).transferConfirmInfo,
+                                style: KaliumStyles.TextStyleParagraphPrimary,
+                                textAlign: TextAlign.left,
+                          )),
+                          Container(
+                              margin: EdgeInsets.symmetric(horizontal: smallScreen(context)?35:60),
+                              child: Text(
+                                KaliumLocalization.of(context).transferConfirmInfoSecond,
+                                style: KaliumStyles.TextStyleParagraph,
+                                textAlign: TextAlign.left,
+                          )),
+                          Container(
+                              margin: EdgeInsets.symmetric(horizontal: smallScreen(context)?35:60),
+                              child: Text(
+                                KaliumLocalization.of(context).transferConfirmInfoThird,
+                                style: KaliumStyles.TextStyleParagraph,
+                                textAlign: TextAlign.left,
+                          )),
                         ],
                       ),
                     ),
-                  ],
-                ),
-              );
-            });
+                  ),
+                  Container(
+                    child: Column(
+                      children: <Widget>[
+                        Row(
+                          children: <Widget>[
+                            // Send Button
+                            KaliumButton.buildKaliumButton(
+                                KaliumButtonType.PRIMARY,
+                                KaliumLocalization.of(context).confirm.toUpperCase(),
+                                Dimens.BUTTON_TOP_DIMENS, onPressed: () {
+                                  KaliumTransferCompleteSheet().mainBottomSheet(context);
+                            }),
+                          ],
+                        ),
+                        Row(
+                          children: <Widget>[
+                            // Scan QR Code Button
+                            KaliumButton.buildKaliumButton(
+                                KaliumButtonType.PRIMARY_OUTLINE,
+                                KaliumLocalization.of(context).cancel.toUpperCase(),
+                                Dimens.BUTTON_BOTTOM_DIMENS, onPressed: () {
+                              Navigator.of(context).pop();
+                            }),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            );
           });
-    });
+        });
   }
 }
--- lib/ui/transfer/transfer_overview_sheet.dart
+++ lib/ui/transfer/transfer_overview_sheet.dart
@@ -4,8 +4,8 @@ import 'package:kalium_wallet_flutter/colors.dart';
 import 'package:kalium_wallet_flutter/kalium_icons.dart';
 import 'package:kalium_wallet_flutter/localization.dart';
 import 'package:kalium_wallet_flutter/dimens.dart';
-import 'package:kalium_wallet_flutter/model/vault.dart';
 import 'package:kalium_wallet_flutter/ui/transfer/transfer_confirm_sheet.dart';
+import 'package:kalium_wallet_flutter/ui/transfer/transfer_manual_entry_sheet.dart';
 import 'package:kalium_wallet_flutter/ui/widgets/auto_resize_text.dart';
 import 'package:kalium_wallet_flutter/ui/widgets/sheets.dart';
 import 'package:kalium_wallet_flutter/ui/widgets/buttons.dart';
@@ -15,119 +15,117 @@ class KaliumTransferOverviewSheet {
   Widget transferFundsStart =
       SvgPicture.asset('assets/transferfunds_illustration_start.svg');
   mainBottomSheet(BuildContext context) {
-    Vault.inst.getSeed().then((result) {
-      KaliumSheets.showKaliumHeightNineSheet(
-          context: context,
-          builder: (BuildContext context) {
-            return StatefulBuilder(
-                builder: (BuildContext context, StateSetter setState) {
-              return Container(
-                width: double.infinity,
-                child: Column(
-                  mainAxisSize: MainAxisSize.max,
-                  children: <Widget>[
-                    // A container for the header
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        //Close Button
-                        Container(
-                          width: 50,
-                          height: 50,
-                          margin: EdgeInsets.only(top: 10.0, left: 10.0),
-                          child: FlatButton(
-                            onPressed: () {
-                              Navigator.pop(context);
-                            },
-                            child: Icon(KaliumIcons.close,
-                                size: 16, color: KaliumColors.text),
-                            padding: EdgeInsets.all(17.0),
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(100.0)),
-                            materialTapTargetSize: MaterialTapTargetSize.padded,
-                          ),
+    KaliumSheets.showKaliumHeightNineSheet(
+        context: context,
+        builder: (BuildContext context) {
+          return StatefulBuilder(
+              builder: (BuildContext context, StateSetter setState) {
+            return Container(
+              width: double.infinity,
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                children: <Widget>[
+                  // A container for the header
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      //Close Button
+                      Container(
+                        width: 50,
+                        height: 50,
+                        margin: EdgeInsets.only(top: 10.0, left: 10.0),
+                        child: FlatButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          child: Icon(KaliumIcons.close,
+                              size: 16, color: KaliumColors.text),
+                          padding: EdgeInsets.all(17.0),
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(100.0)),
+                          materialTapTargetSize: MaterialTapTargetSize.padded,
                         ),
-                        // The header
-                        Container(
-                          margin: EdgeInsets.only(top: 30.0),
-                          constraints: BoxConstraints(maxWidth: MediaQuery.of(context).size.width-140),
-                          child: AutoSizeText(
-                            KaliumLocalization.of(context)
-                                .transferHeader
-                                .toUpperCase(),
-                            style: KaliumStyles.textStyleHeader(context),
-                            textAlign: TextAlign.center,
-                            maxLines: 2,
-                            stepGranularity: 0.1,
-                          ),
-                        ),
-                        // Emtpy SizedBox
-                        SizedBox(
-                          height: 60,
-                          width: 60,
-                        ),
-                      ],
-                    ),
-
-                    // A container for the illustration and paragraphs
-                    Expanded(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: <Widget>[
-                          Container(
-                            constraints: BoxConstraints(
-                                maxHeight:
-                                    MediaQuery.of(context).size.height * 0.2,
-                                maxWidth:
-                                    MediaQuery.of(context).size.width * 0.6),
-                            child: transferFundsStart,
-                          ),
-                          Container(
-                              alignment: Alignment(-1, 0),
-                              margin: EdgeInsets.symmetric(
-                                  horizontal: smallScreen(context)?35:60, vertical: 20),
-                              child: Text(
-                                KaliumLocalization.of(context).transferIntro,
-                                style: KaliumStyles.TextStyleParagraph,
-                                textAlign: TextAlign.left,
-                              )),
-                        ],
                       ),
-                    ),
+                      // The header
+                      Container(
+                        margin: EdgeInsets.only(top: 30.0),
+                        constraints: BoxConstraints(maxWidth: MediaQuery.of(context).size.width-140),
+                        child: AutoSizeText(
+                          KaliumLocalization.of(context)
+                              .transferHeader
+                              .toUpperCase(),
+                          style: KaliumStyles.textStyleHeader(context),
+                          textAlign: TextAlign.center,
+                          maxLines: 2,
+                          stepGranularity: 0.1,
+                        ),
+                      ),
+                      // Emtpy SizedBox
+                      SizedBox(
+                        height: 60,
+                        width: 60,
+                      ),
+                    ],
+                  ),
 
-                    Row(
+                  // A container for the illustration and paragraphs
+                  Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
                       children: <Widget>[
-                        KaliumButton.buildKaliumButton(
-                          KaliumButtonType.PRIMARY,
-                          KaliumLocalization.of(context).scanQrCode,
-                          Dimens.BUTTON_TOP_DIMENS,
-                          onPressed: () {
-                            KaliumTransferConfirmSheet()
-                                .mainBottomSheet(context);
-                          },
+                        Container(
+                          constraints: BoxConstraints(
+                              maxHeight:
+                                  MediaQuery.of(context).size.height * 0.2,
+                              maxWidth:
+                                  MediaQuery.of(context).size.width * 0.6),
+                          child: transferFundsStart,
                         ),
+                        Container(
+                            alignment: Alignment(-1, 0),
+                            margin: EdgeInsets.symmetric(
+                                horizontal: smallScreen(context)?35:60, vertical: 20),
+                            child: Text(
+                              KaliumLocalization.of(context).transferIntro,
+                              style: KaliumStyles.TextStyleParagraph,
+                              textAlign: TextAlign.left,
+                            )),
                       ],
                     ),
-                    Row(
-                      children: <Widget>[
-                        KaliumButton.buildKaliumButton(
-                          KaliumButtonType.PRIMARY_OUTLINE,
-                          "Manual Entry",
-                          Dimens.BUTTON_BOTTOM_DIMENS,
-                          onPressed: () {
-                            KaliumTransferConfirmSheet()
-                                .mainBottomSheet(context);
-                          },
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              );
-            });
+                  ),
+
+                  Row(
+                    children: <Widget>[
+                      KaliumButton.buildKaliumButton(
+                        KaliumButtonType.PRIMARY,
+                        KaliumLocalization.of(context).scanQrCode,
+                        Dimens.BUTTON_TOP_DIMENS,
+                        onPressed: () {
+                          KaliumTransferConfirmSheet()
+                              .mainBottomSheet(context);
+                        },
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: <Widget>[
+                      KaliumButton.buildKaliumButton(
+                        KaliumButtonType.PRIMARY_OUTLINE,
+                        "Manual Entry",
+                        Dimens.BUTTON_BOTTOM_DIMENS,
+                        onPressed: () {
+                          KaliumTransferManualEntrySheet()
+                              .mainBottomSheet(context);
+                        },
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            );
           });
-    });
+        });
   }
 }
