e3ca006f23957cb25a92daa77aeccc162f12a19e
yekta <ngyekta@gmail.com>
2019-01-26 19:46:28 +0300

    New settings to contacts animation + slidable contact added

--- lib/colors.dart
+++ lib/colors.dart
@@ -32,6 +32,10 @@ class KaliumColors {
   static const white15 = Color(0x26FFFFFF);
   static const white03 = Color(0x08FFFFFF);
 
+  static const black15 = Color(0x26000000);
+  static const black20 = Color(0x33000000);
+  static const black30 = Color(0x4D000000);
+  static const black40 = Color(0x66000000);
   static const black50 = Color(0x80000000);
   static const black70 = Color(0xB3000000);
   static const black80 = Color(0xCC000000);
@@ -71,6 +75,9 @@ class KaliumColors {
   static const text15 = white15;
   static const text03 = white03;
 
+  static const overlay15= black15;
+  static const overlay20= black20;
+  static const overlay30= black30;
   static const overlay50= black50;
   static const overlay70= black70;
   static const overlay80= black80;
