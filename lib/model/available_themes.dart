bc1a370bc36c8eeb772e4c88e475bb17c05da376
yekta <ngyekta@gmail.com>
2019-02-21 00:04:46 +0300

    Neptunium theme added

--- lib/model/available_themes.dart
+++ lib/model/available_themes.dart
@@ -3,7 +3,7 @@ import 'package:natrium_wallet_flutter/localization.dart';
 import 'package:natrium_wallet_flutter/themes.dart';
 import 'package:natrium_wallet_flutter/model/setting_item.dart';
 
-enum ThemeOptions { NATRIUM, TITANIUM, INDIUM }
+enum ThemeOptions { NATRIUM, TITANIUM, INDIUM, NEPTUNIUM }
 
 /// Represent notification on/off setting
 class ThemeSetting extends SettingSelectionItem {
@@ -13,6 +13,8 @@ class ThemeSetting extends SettingSelectionItem {
 
   String getDisplayName(BuildContext context) {
     switch (theme) {
+      case ThemeOptions.NEPTUNIUM:
+        return "Neptunium";
       case ThemeOptions.INDIUM:
         return "Indium";
       case ThemeOptions.TITANIUM:
@@ -25,6 +27,8 @@ class ThemeSetting extends SettingSelectionItem {
 
   BaseTheme getTheme() {
     switch (theme) {
+      case ThemeOptions.NEPTUNIUM:
+        return NeptuniumTheme();
       case ThemeOptions.INDIUM:
         return IndiumTheme();
       case ThemeOptions.TITANIUM:
--- lib/themes.dart
+++ lib/themes.dart
@@ -304,7 +304,77 @@ class IndiumTheme extends BaseTheme {
   AppIconEnum appIcon = AppIconEnum.INDIUM;
 }
 
-enum AppIconEnum { NATRIUM, TITANIUM, INDIUM }
+class NeptuniumTheme extends BaseTheme {
+  static const blue = Color(0xFF4A90E2);
+
+  static const orange = Color(0xFFF9AE42);
+
+  static const orangeDark = Color(0xFF9C671E);
+
+  static const blueDark = Color(0xFF000034);
+
+  static const blueLightish = Color(0xFF080840);
+
+  static const blueDarkest = Color(0xFF000034);
+
+  static const white = Color(0xFFFFFFFF);
+
+  static const black = Color(0xFF000000);
+
+  Color primary = blue;
+  Color primary60 = blue.withOpacity(0.6);
+  Color primary45 = blue.withOpacity(0.45);
+  Color primary30 = blue.withOpacity(0.3);
+  Color primary20 = blue.withOpacity(0.2);
+  Color primary15 = blue.withOpacity(0.15);
+  Color primary10 = blue.withOpacity(0.1);
+
+  Color success = orange;
+  Color success60 = orange.withOpacity(0.6);
+  Color success30 = orange.withOpacity(0.3);
+  Color success15 = orange.withOpacity(0.15);
+
+  Color successDark = orangeDark;
+  Color successDark30 = orangeDark.withOpacity(0.3);
+
+  Color background = blueDark;
+  Color background40 = blueDark.withOpacity(0.4);
+  Color background00 = blueDark.withOpacity(0.0);
+
+  Color backgroundDark = blueLightish;
+  Color backgroundDark00 = blueLightish.withOpacity(0.0);
+
+  Color backgroundDarkest = blueDarkest;
+
+  Color text = white.withOpacity(0.9);
+  Color text60 = white.withOpacity(0.6);
+  Color text45 = white.withOpacity(0.45);
+  Color text30 = white.withOpacity(0.3);
+  Color text20 = white.withOpacity(0.2);
+  Color text15 = white.withOpacity(0.15);
+  Color text03 = white.withOpacity(0.03);
+
+  Color overlay90 = black.withOpacity(0.9);
+  Color overlay85 = black.withOpacity(0.85);
+  Color overlay80 = black.withOpacity(0.8);
+  Color overlay70 = black.withOpacity(0.7);
+  Color overlay50 = black.withOpacity(0.5);
+  Color overlay30 = black.withOpacity(0.3);
+  Color overlay20 = black.withOpacity(0.2);
+
+  Brightness brightness = Brightness.dark;
+  SystemUiOverlayStyle statusBar =
+      SystemUiOverlayStyle.light.copyWith(statusBarColor: Colors.transparent);
+
+  BoxShadow boxShadow = BoxShadow(color: Colors.transparent);
+  BoxShadow boxShadowButton = BoxShadow(color: Colors.transparent);
+
+  OverlayTheme qrScanTheme = OverlayTheme.NATRIUM;
+  FPDialogTheme fpTheme = FPDialogTheme.NATRIUM;
+  AppIconEnum appIcon = AppIconEnum.NEPTUNIUM;
+}
+
+enum AppIconEnum { NATRIUM, TITANIUM, INDIUM, NEPTUNIUM }
 class AppIcon {
   static const _channel = const MethodChannel('fappchannel');
 
@@ -314,6 +384,9 @@ class AppIcon {
     }
     String iconStr = "natrium";
     switch (iconToChange) {
+      case AppIconEnum.NEPTUNIUM:
+        iconStr = "neptunium";
+        break;
       case AppIconEnum.INDIUM:
         iconStr = "indium";
         break;
