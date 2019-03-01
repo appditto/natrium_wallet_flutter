2e90bfeb6e3f337112c60d76704290a48b3ab33b
yekta <ngyekta@gmail.com>
2019-03-01 21:20:24 +0300

    Account details sheet name container margin change

--- lib/ui/accounts/accountdetails_sheet.dart
+++ lib/ui/accounts/accountdetails_sheet.dart
@@ -228,7 +228,7 @@ class AccountDetailsSheet {
                           Container(
                             width: double.infinity,
                             margin: EdgeInsets.only(
-                              top: MediaQuery.of(context).size.width * 0.1,
+                              top: MediaQuery.of(context).size.width * 0.12,
                               left: MediaQuery.of(context).size.width * 0.105,
                               right: MediaQuery.of(context).size.width * 0.105,
                             ),
