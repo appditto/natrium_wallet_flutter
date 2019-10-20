17e6d4fe01fe2f3bee437013fbee0ee7b664f390
yekta <ngyekta@gmail.com>
2019-10-20 16:12:05 +0300

    ParseAddressString changed

--- lib/model/address.dart
+++ lib/model/address.dart
@@ -16,21 +16,29 @@ class Address {
 
   String getShortString() {
     if (_address == null || _address.length < 64) return null;
-    return _address.substring(0, 11) + "..." + _address.substring(_address.length - 6);
+    return _address.substring(0, 11) +
+        "..." +
+        _address.substring(_address.length - 6);
   }
+
   String getShorterString() {
     if (_address == null || _address.length < 64) return null;
-    return _address.substring(0, 9) + "..." + _address.substring(_address.length - 4);
+    return _address.substring(0, 9) +
+        "..." +
+        _address.substring(_address.length - 4);
   }
 
   bool isValid() {
-    return _address == null ? false : NanoAccounts.isValid(NanoAccountType.NANO, _address);
+    return _address == null
+        ? false
+        : NanoAccounts.isValid(NanoAccountType.NANO, _address);
   }
 
   void _parseAddressString(String value) {
     if (value != null) {
       value = value.toLowerCase();
-      _address = NanoAccounts.findAccountInString(NanoAccountType.NANO, value);
+      _address = NanoAccounts.findAccountInString(
+          NanoAccountType.NANO, value.replaceAll("\n", ""));
       var split = value.split(':');
       if (split.length > 1) {
         Uri uri = Uri.tryParse(value);
