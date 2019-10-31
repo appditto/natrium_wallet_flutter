537eb9e85f1ca49e74ec0e71199900e1747594df
bbedward <bbedward@gmail.com>
2019-10-31 17:08:21 -0400

    Add deploy stage

--- ci/tag_version.sh
+++ ci/tag_version.sh
@@ -7,13 +7,15 @@ VERSION=$(get_version)
 # See if TAG exists
 
 export GIT_DIR=./.git
-if git rev-parse $VERSION >/dev/null 2>&1
-then
-    echo "Tag already exists"
-    exit 0
-fi
-echo "Tag not found, creating..."
+#if git rev-parse $VERSION >/dev/null 2>&1
+#then
+#    echo "Tag already exists"
+#    exit 0
+#fi
+#echo "Tag not found, creating..."
+git tag -d $VERSION
 git tag $VERSION HEAD
+git push origin :$VERSION
 git push origin $VERSION
 if [ $? -ne 0 ]
 then
