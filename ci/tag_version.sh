25ff74819826d343b3f1d612502301a829cca28e
Brandon Berhent <bbedward@gmail.com>
2019-10-31 15:08:12 -0400

    New ci scripts and android build pipeline

---
+++ ci/tag_version.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+source ./ci/get_version.sh
+
+VERSION=$(get_version)
+
+# See if TAG exists
+
+export GIT_DIR=./.git
+if git rev-parse $VERSION >/dev/null 2>&1
+then
+    echo "Tag already exists"
+    exit 0
+fi
+echo "Tag not found, creating..."
+git tag $VERSION HEAD
+git push origin $VERSION
+if [ $? -ne 0 ]
+then
+    echo "Fatal, failed to push tag $VERSION"
+    exit 1
+fi
---
+++ ci/upload_android_github.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+#
+# Author: Stefan Buck
+# https://gist.github.com/stefanbuck/ce788fee19ab6eb0b4447a85fc99f447
+#
+#
+# This script accepts the following parameters:
+#
+# * owner
+# * repo
+# * tag
+# * filename
+# * github_api_token
+#
+# Script to upload a release asset using the GitHub API v3.
+
+source ./ci/get_version.sh
+VERSION=$(get_version)
+VERSION="v2.1.3"
+
+# Define variables.
+GH_TAGS="$GH_REPO/releases/tags/$VERSION"
+WGET_ARGS="--content-disposition --auth-no-challenge --no-cookie"
+CURL_ARGS="-LJO#"
+filename=./build/app/outputs/apk/release/app-release.apk
+
+if [[ "$tag" == 'LATEST' ]]; then
+  GH_TAGS="$GH_REPO/releases/latest"
+fi
+
+# Validate token.
+curl -o /dev/null -sH "$AUTH" $GH_REPO || { echo "Error: Invalid repo, token or network issue!";  exit 1; }
+
+# Read asset tags.
+response=$(curl -sH "$AUTH" $GH_TAGS)
+echo $response
+# Get ID of the asset based on given filename.
+eval $(echo "$response" | grep -m 1 "id.:" | grep -w id | tr : = | tr -cd '[[:alnum:]]=')
+[ "$id" ] || { echo "Error: Failed to get release id for tag: $VERSION"; echo "$response" | awk 'length($0)<100' >&2; exit 1; }
+
+# Upload asset
+echo "Uploading asset... $localAssetPath" >&2
+
+# Construct url
+GH_ASSET="https://uploads.github.com/repos/$GH_OWNER/$GH_REPO_NAME/releases/$id/assets?name=$(basename $filename)"
+
+curl "$GITHUB_OAUTH_BASIC" --data-binary @"$filename" -H "$AUTH" -H "Content-Type: application/octet-stream" $GH_ASSET
