6ebf4434c3bbfc457d6c4f38128d2006cbede6dc
bbedward <bbedward@gmail.com>
2019-10-31 21:13:35 -0400

    Add ci scrupt to upload APK to github release notes

--- ci/tag_version.sh
+++ ci/tag_version.sh
@@ -3,22 +3,33 @@
 source ./ci/get_version.sh
 
 VERSION=$(get_version)
+GITHUB_OAUTH_BASIC="${GITHUB_API_TOKEN}:x-oauth-basic"
+AUTH="Authorization: token $GITHUB_API_TOKEN"
+GH_PUBLISH="$GH_REPO/releases"
 
 # See if TAG exists
 
-export GIT_DIR=./.git
+#export GIT_DIR=./.git
 #if git rev-parse $VERSION >/dev/null 2>&1
 #then
 #    echo "Tag already exists"
 #    exit 0
 #fi
 #echo "Tag not found, creating..."
-git tag -d $VERSION
-git tag $VERSION HEAD
-git push origin :$VERSION
-git push origin $VERSION
-if [ $? -ne 0 ]
-then
-    echo "Fatal, failed to push tag $VERSION"
-    exit 1
+#git tag -d $VERSION
+#git tag $VERSION HEAD
+#git push origin :$VERSION
+#git push origin $VERSION
+#if [ $? -ne 0 ]
+#then
+#    echo "Fatal, failed to push tag $VERSION"
+#    exit 1
+#fi
+
+pubresponse=$(curl -u "$GITHUB_OAUTH_BASIC" -sH "$AUTH" --data '{"tag_name":"'"$VERSION"'", "name":"Natrium '"${VERSION}"'", "draft":false, "prerelease":false}' $GH_PUBLISH)
+if [[ "$pubresponse" == *"Validation Failed"* ]]; then
+  echo "Error! ${pubresponse}"
+  exit 1
 fi
+echo "Release created"
+exit 0
--- ci/upload_android_github.sh
+++ ci/upload_android_github.sh
@@ -1,31 +1,22 @@
 #!/bin/bash
 #
-# Author: Stefan Buck
+# Based on:
+
 # https://gist.github.com/stefanbuck/ce788fee19ab6eb0b4447a85fc99f447
 #
+# Uploads our android release APK to a corresponding release on github releases page
 #
-# This script accepts the following parameters:
-#
-# * owner
-# * repo
-# * tag
-# * filename
-# * github_api_token
-#
-# Script to upload a release asset using the GitHub API v3.
 
 source ./ci/get_version.sh
 VERSION=$(get_version)
 
 # Define variables.
 GH_TAGS="$GH_REPO/releases/tags/$VERSION"
+GH_PUBLISH="$GH_REPO/releases"
 WGET_ARGS="--content-disposition --auth-no-challenge --no-cookie"
 CURL_ARGS="-LJO#"
 filename=./build/app/outputs/apk/release/app-release.apk
-
-if [[ "$tag" == 'LATEST' ]]; then
-  GH_TAGS="$GH_REPO/releases/latest"
-fi
+GITHUB_OAUTH_BASIC="${GITHUB_API_TOKEN}:x-oauth-basic"
 
 AUTH="Authorization: token $GITHUB_API_TOKEN"
 
@@ -33,16 +24,19 @@ AUTH="Authorization: token $GITHUB_API_TOKEN"
 curl -o /dev/null -sH "$AUTH" $GH_REPO || { echo "Error: Invalid repo, token or network issue!";  exit 1; }
 
 # Read asset tags.
-response=$(curl -sH "$AUTH" $GH_TAGS)
-echo $response
+response=$(curl -u "$GITHUB_OAUTH_BASIC" -sH "$AUTH" $GH_TAGS)
+
 # Get ID of the asset based on given filename.
 eval $(echo "$response" | grep -m 1 "id.:" | grep -w id | tr : = | tr -cd '[[:alnum:]]=')
-[ "$id" ] || { echo "Error: Failed to get release id for tag: $VERSION"; echo "$response" | awk 'length($0)<100' >&2; exit 1; }
-
+[ "$id" ] || {
+  echo "Error: Failed to get release id for tag: $VERSION";
+  echo "$response" awk 'length($0)<100' >&2;
+  exit 1;
+}
 # Upload asset
 echo "Uploading asset... $localAssetPath" >&2
 
 # Construct url
-GH_ASSET="https://uploads.github.com/repos/$GH_OWNER/$GH_REPO_NAME/releases/$id/assets?name=$(basename $filename)"
+GH_ASSET="https://uploads.github.com/repos/$GH_OWNER/$GH_REPO_NAME/releases/$id/assets?name=natrium_${VERSION}.apk"
 
-curl "$GITHUB_OAUTH_BASIC" --data-binary @"$filename" -H "$AUTH" -H "Content-Type: application/octet-stream" $GH_ASSET
+curl -u "$GITHUB_OAUTH_BASIC" --data-binary @"$filename" -H "$AUTH" -H "Content-Type: application/octet-stream" $GH_ASSET
