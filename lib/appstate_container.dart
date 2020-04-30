a41b44a5ab0f81bd539fc258ce926cc31a877fc5
Brandon Berhent <bbedward@gmail.com>
2020-04-30 18:39:50 -0400

    Fix deprecation warning and update dependencies

--- lib/appstate_container.dart
+++ lib/appstate_container.dart
@@ -76,8 +76,7 @@ class StateContainer extends StatefulWidget {
   // Exactly like MediaQuery.of and Theme.of
   // It basically says 'get the data from the widget of this type.
   static StateContainerState of(BuildContext context) {
-    return (context.inheritFromWidgetOfExactType(_InheritedStateContainer)
-            as _InheritedStateContainer).data;
+    return context.dependOnInheritedWidgetOfExactType<_InheritedStateContainer>().data;
   }
   
   @override
