a41eb99cee29955daa8cdaa50270bb9340df4b8e
Brandon Berhent <bbedward@gmail.com>
2020-05-01 13:37:45 -0400

    Limit history for faster loading

--- lib/appstate_container.dart
+++ lib/appstate_container.dart
@@ -606,7 +606,7 @@ class StateContainerState extends State<StateContainer> {
       // This is can still be improved because history excludes change/open, blockCount doesn't
 
       // Get largest count we have + 5 (just a safe-buffer)
-      int count = max(max(response.blockCount, 3000), wallet.history.length) + 5;
+      int count = max(995, max(response.blockCount, wallet.history.length)) + 5;
       // Subtract by what we already have to get amount we want to request
       count -= wallet.history.length;
       // Minimum of 10 to request
