f6e03fd0d971f2eb50bd250071e9133286594de7
bbedward <bbedward@gmail.com>
2021-01-18 10:14:25 -0500

    Switch to flutter embedding v2

--- android/app/src/main/java/co/banano/natriumwallet/MainActivity.java
+++ android/app/src/main/java/co/banano/natriumwallet/MainActivity.java
@@ -2,33 +2,28 @@ package co.banano.natriumwallet;
 
 import android.os.Bundle;
 
-import io.flutter.app.FlutterFragmentActivity;
-import io.flutter.plugin.common.MethodCall;
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterFragmentActivity;
+import io.flutter.embedding.engine.FlutterEngine;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 import io.flutter.view.FlutterMain;
 
 public class MainActivity extends FlutterFragmentActivity {
-  private static final String CHANNEL = "fappchannel";
+    private static final String CHANNEL = "fappchannel";
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    FlutterMain.startInitialization(this);
-    super.onCreate(savedInstanceState);
-
-    GeneratedPluginRegistrant.registerWith(this);
-
-    new MethodChannel(getFlutterView(), CHANNEL).setMethodCallHandler(
-          new MethodChannel.MethodCallHandler() {
-              @Override
-              public void onMethodCall(MethodCall call, MethodChannel.Result result) {
-                  if (call.method.equals("getSecret")) {
-                      result.success(new LegacyStorage().getSecret());                      
-                  } else {
-                      result.notImplemented();
-                  }
-              }
-          }
-     );
-  }
+    @Override
+    public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+        new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL)
+            .setMethodCallHandler(
+                (call, result) -> {
+                    if (call.method.equals("getSecret")) {
+                        result.success(new LegacyStorage().getSecret());
+                    } else {
+                        result.notImplemented();
+                    }
+                }
+            );
+    }
 }
