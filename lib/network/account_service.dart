7c1fbe52e1d3129a1fefff23c6d15e196450769e
bbedward <bbedward@gmail.com>
2021-03-15 14:18:03 -0400

    Test URL

--- lib/network/account_service.dart
+++ lib/network/account_service.dart
@@ -39,8 +39,8 @@ import 'package:natrium_wallet_flutter/network/model/response/process_response.d
 import 'package:natrium_wallet_flutter/bus/events.dart';
 
 // Server Connection String
-const String _SERVER_ADDRESS = "wss://app.natrium.io";
-const String _SERVER_ADDRESS_HTTP = "https://app.natrium.io/api";
+const String _SERVER_ADDRESS = "wss://testapp.natrium.io";
+const String _SERVER_ADDRESS_HTTP = "https://testapp.natrium.io/api";
 
 Map decodeJson(dynamic src) {
   return json.decode(src);
